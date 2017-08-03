e3b344c6d05d8531cb71e7fde51f9bc240194811
gurgen16 <rommesbob@hotmail.com>
2017-08-03 14:26:32 +0200

    Mehrere Verläufe bei Filtertests auf einmal darstellbar

--- Filtertests.py
+++ Filtertests.py
@@ -4,41 +4,60 @@ import Tkinter
 import tkFileDialog as fd
 import scipy
 import scipy.signal as signal
-# import os.path
+import os.path
 
 plt.close('all')
 
 
-# pfad='C:\Users\Bob-Lap-\Google Drive\Transfer\Python\Ye.txt' # Amplitudengang aus Matlab für 2,8mb Datei
-# pfad=r"C:\Files\Programming\Ye.txt"
-# os.path.isfile(pfad)
-#"C:\Users\Bob-Lap\Google Drive\Transfer\Python\Ye.txt"
-
-
-asdDir=r'C:\Users\Bob-Lap\Google Drive\Transfer\Python\Testordner'
-
-datei=fd.askopenfile(initialdir=asdDir,title='Messdatei auswählen')
-stoss=np.loadtxt(datei.name)
-datei.close()
-
-
-
-def filt(verlauf):
-
-    b, a = signal.ellip(4, 0.01, 120, 0.5)
-    out=signal.filtfilt(b,a,verlauf)
+## Filter-Funktion
+def filt(sig):
+    b, a = signal.ellip(4, 0.01, 120, 0.125)
+    out = signal.filtfilt(b, a, sig)
     return out
 
-filt(stoss)
 
-plt.figure(1)
+## Dateien einlesen und Funktion anwenden
+# asdDir=r"D:\Files\Backup\Google Drive\Transfer\Python"
+asdDir = r"C:\Users\Bob-Lap\Google Drive\Transfer\Python"
+dateilist = fd.askopenfiles(initialdir=asdDir, title='Messdatei auswählen')
 
-plt.subplot(211)
-plt.semilogy(stoss)
-plt.title('Roh-Verlauf')
+verlauf = np.zeros(np.size(dateilist))
+filverlauf = np.zeros(np.size(dateilist))
 
-plt.subplot(212)
-plt.semilogy(out)
-plt.title('Gefilterter-Verlauf')
+i = 0
+for datei in dateilist:  # Durchläuft alle Dateien
+
+    # verlauf[i]=np.loadtxt(datei.name)   # Lädt Werte
+    verlauf = np.loadtxt(datei.name)  # Lädt Werte
+
+    datei.close()
+    # filverlauf[i]=filt(verlauf[i]) # Filter-Funktion
+    filverlauf = filt(verlauf)
+    i += 1
+
+    ## Darstellung
+    # if np.size(dateilist)==1
+    # j=0
+    # for fil in filverlauf:
+
+    plt.figure()
+    plt.suptitle(os.path.basename(datei.name))
+
+    plt.subplot(211)
+    # plt.semilogy(verlauf[j])
+    plt.semilogy(verlauf)
+    plt.title('Roh-Verlauf')
+
+    plt.subplot(212)
+    # plt.semilogy(filverlauf[j])
+    plt.semilogy(filverlauf)
+    plt.title('Gefilterter-Verlauf')
+
+    savepath = os.path.dirname(datei.name)
+    savename = '\\filter' + os.path.basename(datei.name)[5:-4] + '.asd'
+
+    with open(savepath + savename, "w") as outo:
+        np.savetxt(savepath + savename, filverlauf)
+        # j+=0
 
 plt.show()
